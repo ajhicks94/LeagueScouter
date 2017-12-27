79aabd019091f2de29b6e8da515b4aadc63fb4ae
Anthony Hicks <ajhicks94@gmail.com>
2017-12-27 15:53:27 -0600

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { 
+    title: 'Express',
+    age: '23'
+  });
+});
+
+module.exports = router;
